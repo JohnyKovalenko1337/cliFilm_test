10b59ed24821a93b329252a6c2e76839d947e871
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-12-30 23:44:09 +0200

    finished server routes

--- server/routes/film-routes.js
+++ server/routes/film-routes.js
@@ -1,13 +1,32 @@
 const express = require('express');
 
 const {
-    addFilm
-    } = require('../controllers/film-controllers');
+    getFilms,
+    addFilm,
+    getFilmById,
+    deleteById,
+    filmByName,
+    filmByActor,
+    importFromFile
+} = require('../controllers/film-controllers');
+
+const { addFilmSchema } = require('../validation/addFilm');
 
 const router = express.Router();
 
-router.get('/add', addFilm);
+router.get('/film', getFilms);
 
+router.post('/add', addFilmSchema, addFilm);
 
+router.get('/film/:id', getFilmById);
+
+router.delete('/deleteById/:id', deleteById);
+
+router.get('/film/name/:name', filmByName);
+
+router.get('/film/actor/:actor', filmByActor);
+
+router.get('/film/file/import', importFromFile);
 
 module.exports = router;
+
